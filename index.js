e3eed791b82cfdedd4f10fb264bf138eb7e4ac2e
Rajeshkanth <rajeshkanth@solinas.in>
2025-11-28 00:33:34 +0530

    code cleanup & cors added

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 import dotenv from "dotenv";
 import express from "express";
 import fetch from "node-fetch";
+import cors from "cors";
+
 dotenv.config();
 
 const app = express();
+app.use(cors());
 
 const scope = "user-read-currently-playing user-read-playback-state";
 const redirectUri = encodeURIComponent(process.env.SPOTIFY_REDIRECT_URI);
 const clientId = process.env.SPOTIFY_CLIENT_ID;
 const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+const BASE_ACC_URL = "https://accounts.spotify.com/";
+const SPOTIFY_API_URL = "https://api.spotify.com/v1/";
 
 app.get("/login", (req, res) => {
   const url =
-    `https://accounts.spotify.com/authorize?response_type=code` +
+    `${BASE_ACC_URL}authorize?response_type=code` +
     `&client_id=${clientId}` +
     `&scope=${encodeURIComponent(scope)}` +
     `&redirect_uri=${redirectUri}`;
@@ -24,9 +29,8 @@ app.get("/callback", async (req, res) => {
   const code = req.query.code;
 
   const basic = Buffer.from(`${clientId}:${clientSecret}`).toString("base64");
-console.log("redirect uri: ", decodeURIComponent(redirectUri));
 
-  const tokenResponse = await fetch("https://accounts.spotify.com/api/token", {
+  const tokenResponse = await fetch(`${BASE_ACC_URL}api/token`, {
     method: "POST",
     headers: {
       Authorization: `Basic ${basic}`,
@@ -41,9 +45,6 @@ console.log("redirect uri: ", decodeURIComponent(redirectUri));
 
   const data = await tokenResponse.json();
 
-  console.log(data);
-  
-
   console.log("Access Token: ", data.access_token);
   console.log("Refresh Token: ", data.refresh_token);
 
@@ -53,7 +54,7 @@ console.log("redirect uri: ", decodeURIComponent(redirectUri));
 const getAccessToken = async () => {
   const basic = Buffer.from(`${clientId}:${clientSecret}`).toString("base64");
 
-  const response = await fetch("https://accounts.spotify.com/api/token", {
+  const response = await fetch(`${BASE_ACC_URL}api/token`, {
     method: "POST",
     headers: {
       Authorization: `Basic ${basic}`,
@@ -74,7 +75,7 @@ app.get("/now-playing", async (req, res) => {
     const access_token = await getAccessToken();
 
     const response = await fetch(
-      "https://api.spotify.com/v1/me/player/currently-playing",
+      `${SPOTIFY_API_URL}me/player/currently-playing`,
       {
         headers: {
           Authorization: `Bearer ${access_token}`,
@@ -111,6 +112,7 @@ app.get("/now-playing", async (req, res) => {
 });
 
 app.listen(process.env.PORT, () => {
-    console.log("Server is up and running on port ,", process.env.PORT);
-    
-})
+  console.log("## ## ## ## ## ##");
+  console.log("Server is up and running on port ,", process.env.PORT);
+  console.log("## ## ## ## ## ##");
+});
